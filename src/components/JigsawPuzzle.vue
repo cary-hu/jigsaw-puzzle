bc502570649f11446aec69c2bc4bcc7c5b212290
Cary hu <cary.hu@grapecity.com>
2022-03-18 22:19:34 +0800

    feat: support reset board

--- src/components/JigsawPuzzle.vue
+++ src/components/JigsawPuzzle.vue
@@ -1,19 +1,40 @@
 <script setup lang="ts">
-import { computed, ref, watch } from "vue";
+import { computed, ref } from "vue";
+import draggable from "vuedraggable";
 import image from "../assets/default.png";
 import { GamePlay } from "./logic";
-
-const play = new GamePlay(6);
-
-const n = ref(6);
+const n = ref(4);
+const play = new GamePlay(n.value);
 
 const width = computed(() => {
   return 600 / n.value;
 });
 
 const state = computed(() => play.state.value);
+const simpleBoard = computed(() => state.value.board.flat());
+const isPlay = ref(false);
 const beginGame = () => {
+  isPlay.value = true;
   play.shuffle();
+};
+const resetGame = () => {
+  isPlay.value = false;
+  play.reset(n.value);
+};
+
+const upGameLevel = () => {
+  if (n.value < 18)
+    n.value += 2;
+  else
+    return;
+  play.reset(n.value);
+};
+const downGameLevel = () => {
+  if (n.value > 2)
+    n.value -= 2;
+  else
+    return;
+  play.reset(n.value);
 };
 
 </script>
@@ -21,23 +42,23 @@ const beginGame = () => {
 <template>
   <div class="warp">
     <div>
-      <div class="btn" @click="play.reset(++n)">
+      <div class="btn" @click="upGameLevel">
         增加游戏难度
       </div>
-      <div class="btn" @click="play.reset(--n)">
+      <div class="btn" @click="downGameLevel">
         降低游戏难度
       </div>
-      <div class="btn" @click="beginGame">
+      <div v-if="!isPlay" class="btn" @click="beginGame">
         开始游戏
       </div>
+      <div v-else class="btn" @click="resetGame">
+        重置
+      </div>
     </div>
-    <div class="board">
-      <template v-for="(col, x) in state.board" :key="x">
-        <template v-for="(row, y) in col" :key="y">
-          <div class="jigsaw-block" :style="{width: width + 'px', height: width + 'px', backgroundImage: 'url(' + image + ')', backgroundPositionX: (row.y * (100 / (n - 1))) + '%', backgroundPositionY: (row.x * (100 / (n - 1))) + '%'}" />
-        </template>
-      </template>
-    </div>
+    <TransitionGroup name="list" tag="ul" class="board">
+      <li v-for="(row, index) in simpleBoard" :key="row.x + row.y + index * (Math.random())" class="jigsaw-block" :class="isPlay ? 'play' : ''" :data-origin-y="row.originY" :data-origin-x="row.originX" :style="{width: width + 'px', height: width + 'px', backgroundImage: 'url(' + image + ')', backgroundPositionX: (row.y * (100 / (n - 1))) + '%', backgroundPositionY: (row.x * (100 / (n - 1))) + '%'}" />
+    </TransitionGroup>
+    <!-- <draggable v-model="simpleBoard" tag="transition-group" :component-data="{name:'list'}" class="board" /> -->
   </div>
 </template>
 
@@ -47,17 +68,28 @@ const beginGame = () => {
     flex-direction: column;
 }
 .board {
+    position: relative;
     background-color: #000;
     padding: 12px;
-    width: 620px;
-    height: 620px;
+    width: 600px;
+    height: 600px;
     display: flex;
     flex-wrap: wrap;
+    list-style: none;
 }
 .jigsaw-block {
     background-size: 600px 600px;
     background-repeat: no-repeat;
-    border: 1px solid #000;
+    color: #000;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+.jigsaw-block.play::after {
+  content: "";
+  border: 1px solid #000;
+  width: 100%;
+  height: 100%;
 }
 .btn {
     padding: 4px 8px;
@@ -66,5 +98,29 @@ const beginGame = () => {
     margin: 4px;
     border-radius: 4px;
     cursor: pointer;
+    user-select: none;
+}
+
+/* 1. declare transition */
+.list-move,
+.list-enter-active,
+.list-leave-active {
+  transition: all 0.5s ease;
+}
+
+/* 2. declare enter from and leave to state */
+.list-leave-to {
+  opacity: 0;
+  transform: translateY(-20px);
+}
+.list-enter-from {
+  opacity: 0;
+  transform: translateY(20px);
+}
+
+/* 3. ensure leaving items are taken out of layout flow so that moving
+      animations can be calculated correctly. */
+.list-leave-active {
+  position: absolute;
 }
 </style>
--- src/components/logic.ts
+++ src/components/logic.ts
@@ -1,42 +1,65 @@
 import type { Ref } from "vue";
 import { ref } from "vue";
+
 interface BlockState {
   x: number
   y: number
+  originX: number
+  originY: number
+  swapped: boolean
 }
 interface JigsawBlock {
   board: BlockState[][]
 }
 export class GamePlay {
+  private _currentN = 6;
   constructor(n: number) {
     this.reset(n);
   }
 
   state = ref() as Ref<JigsawBlock>;
   reset(n: number) {
+    this._currentN = n;
     this.state.value = {
       board: Array.from({ length: n }, (_, x) =>
         Array.from({ length: n }, (_, y) => ({
           x,
           y,
+          originX: x,
+          originY: y,
+          swapped: false,
         }))),
     };
   }
 
   shuffle() {
     const block = this.state.value.board;
-    for (let k = 0; k < block.length; k++) {
-      let i = block[k].length;
-      if (i === 0) { return false; }
-      else {
-        while (--i) {
-          const j = Math.floor(Math.random() * (i + 1));
-          const tempi = block[k][i];
-          const tempj = block[k][j];
-          block[k][i] = tempj;
-          block[k][j] = tempi;
-        }
+    for (let k = 0; k < this._currentN; k++) {
+      for (let i = 0; i < this._currentN; i++) {
+        const element = block[k][i];
+        if (element.swapped)
+          continue;
+
+        element.swapped = true;
+        const { x, y } = this.getRandomNumber(this._currentN, k, i);
+        const swapItem = block[x][y];
+        element.x = swapItem.originX;
+        element.y = swapItem.originY;
+        swapItem.x = element.originX;
+        swapItem.y = element.originY;
+        swapItem.swapped = true;
       }
     }
+  }
+
+  getRandomNumber(n: number, notX: number, notY: number): { x: number; y: number } {
+    const block = this.state.value.board;
+
+    const randomNumberX = Math.floor(Math.random() * n);
+    const randomNumberY = Math.floor(Math.random() * n);
+    if (block[randomNumberX][randomNumberY].swapped)
+      return this.getRandomNumber(n, notX, notY);
+    else
+      return { x: randomNumberX, y: randomNumberY };
   }
 }
